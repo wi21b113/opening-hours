5e50965587ab93c775cf9a8f58eb4ce3ce6f636f
Herwig Brunnthaler <wi21b113@technikum-wien.at>
2022-09-30 01:09:10 +0200

    Initial commit

---
+++ src/main/java/at/technikum/openinghours/OpeningHoursApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.openinghours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpeningHoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpeningHoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/technikum/openinghours/OpeningHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.openinghours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpeningHoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
